c443e0bb5b9345f188d56d0aa9edcc7ee21e21b6
nohannah <67176677+nohannah@users.noreply.github.com>
2022-09-01 16:59:17 +0700

    18/03/2022

---
+++ week9.py
@@ -0,0 +1,29 @@
+# for i in range(2,200):
+
+#   if i%2==0:
+
+#     print(i)
+
+# print("num is:",i)
+
+# for i in range(1,200):
+#     if i%2!=0:
+#         print(i)
+# print("num is:",i)
+#sort number
+
+# number = [9,1,8,2,7,3,6,4,5] 
+# number.sort()
+# print(number)
+
+list = [1,2,3,4,5,6,7,8,9,] 
+
+for num in list: 
+
+  if (num % 2 == 0):
+    print(num)
+    
+list =[1,2,3,4,5,6,7,8,9]
+for num in list :
+    num *=2
+    print(num)
